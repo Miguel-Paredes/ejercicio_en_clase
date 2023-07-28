bef8bb02d95d475820b11afd16ab85caed87df64
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-07-28 11:40:26 -0500

    update

--- src/login.java
+++ src/login.java
@@ -1,18 +1,34 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class login {
     private JPanel JPanel;
     private JButton ingresarButton;
     private JTextField usuario;
     private JPasswordField contrasenia;
+    static final String DB_RUL = "jdbc:mysql://localhost/poo";
+    static final String USER = "root";
+    static final String PASS = "root_bas3";
+    static final String QUERY = "Select * From Estudiantes";
 
     public login() {
         ingresarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                try {
+                    boolean login=false;
+                    Connection conn = DriverManager.getConnection(DB_RUL, USER, PASS);
+                    Statement stmt = conn.createStatement();
+                    ResultSet rs = stmt.executeQuery(QUERY);
+                    if(rs.getString("nombre")=usuario && contrasenia=rs.getString("password")){
+                        login=true;
+                    }
+                }
+                catch (SQLException e){
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
