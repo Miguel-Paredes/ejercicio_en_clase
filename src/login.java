3946fa1458e3b5680edfa0e0067a6c29de497185
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-07-28 11:31:05 -0500

    update

--- src/login.java
+++ src/login.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class login {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class login {
+    private JPanel JPanel;
+    private JButton ingresarButton;
+    private JTextField usuario;
+    private JPasswordField contrasenia;
+
+    public login() {
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("login");
+        frame.setContentPane(new login().JPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
